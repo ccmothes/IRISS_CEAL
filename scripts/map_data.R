871d04ad03cfc65aac287705be402530b3f87657
Caitlin Mothes <ccmothes@gmail.com>
2021-12-16 10:14:29 -0700

    clean up symbology of maps

--- scripts/map_data.R
+++ scripts/map_data.R
@@ -25,7 +25,7 @@ pc_sp <- pc %>% filter(!is.na(LATITUDE) | LATITUDE != 0) %>%
 # filter out points not in desired counties and jitter
 
 pc_filtered <- st_intersection(pc_sp, county_refined) %>% 
-  st_jitter(amount = 0.0001, factor = 0.001) # I think default value is fine, just know addresses are off by a couple streets
+  st_jitter(amount = 0.0005, factor = 0.001) # I think default value is fine, just know addresses are off by a couple streets
 
 # investigate
 leaflet() %>% 
@@ -49,11 +49,19 @@ leaflet() %>%
   #addPolygons(data = county, color = "black", fillOpacity = 0, stroke = 0.1, popup = paste("County:", county$NAME)) %>% 
   addPolygons(data = county_refined, col = "black", fillOpacity = 0, opacity = 0.9,
               popup = paste("County:", county_refined$NAME)) %>% 
-  addCircleMarkers(data = pc_filtered, radius = 2, opacity = 1,
-                   color = ~pal_group(NTMAJ10),
+  addCircleMarkers(data = pc_filtered, radius = 6, opacity = 1,
+                   color = "black",
+                   stroke = TRUE,
+                   weight = 0.2,
+                   fillOpacity = 1,
+                   fillColor = ~pal_group(NTMAJ10),
                    popup = paste("Name:", pc_filtered$NAME, "<br>",
                                  "Address:", pc_filtered$full_address, "<br>",
-                                 "Major Group:", pc_filtered$NTMAJ10)) %>% 
+                                 "Major Group:", pc_filtered$NTMAJ10, "<br>",
+                                 "Contributions:", paste0("$", pc_filtered$CONT), "<br>",
+                                 "Revenue:", paste0("$",pc_filtered$TOTREV), "<br>",
+                                 "Expenses:", paste0("$",pc_filtered$EXPS), "<br>",
+                                 "Year: 2018")) %>% 
   addLegend(pal = pal_group, values = pc_filtered$NTMAJ10, opacity = 0.9,
             labFormat = function(type, cuts, p) {  # Here's the trick
               paste0(labels_group)
@@ -63,28 +71,38 @@ leaflet() %>%
 
 # color by total revenue (calculated, TOTREV)
 
-pal_rev <- colorQuantile(palette = "RdYlBu", reverse = TRUE,domain = pc_filtered$TOTREV, n = 20)
+pal_rev <- colorQuantile(palette = "RdYlBu", reverse = TRUE,domain = pc_filtered$TOTREV, n = 10)
 
 leaflet() %>% 
   addTiles() %>% 
   #addPolygons(data = county, color = "black", fillOpacity = 0, stroke = 0.1, popup = paste("County:", county$NAME)) %>% 
   addPolygons(data = county_refined, col = "black", fillOpacity = 0, opacity = 0.9,
               popup = paste("County:", county_refined$NAME)) %>% 
-  addCircleMarkers(data = pc_filtered, radius = 2, opacity = 1,
-                   color = ~pal_rev(TOTREV),
+  addCircleMarkers(data = pc_filtered, radius = 6, opacity = 1,
+                   color = "black",
+                   stroke = TRUE,
+                   weight = 0.2,
+                   fillOpacity = 1,
+                   fillColor = ~pal_rev(TOTREV),
                    popup = paste("Name:", pc_filtered$NAME, "<br>",
                                  "Address:", pc_filtered$full_address, "<br>",
-                                 "Annual Revenue:", paste0("$",pc_filtered$TOTREV))) %>% 
-  addLegend(pal = pal_rev, values = pc_filtered$TOTREV, opacity = 0.9)
+                                 "Major Group:", pc_filtered$NTMAJ10, "<br>",
+                                 "Contributions:", paste0("$", pc_filtered$CONT), "<br>",
+                                 "Revenue:", paste0("$",pc_filtered$TOTREV), "<br>",
+                                 "Expenses:", paste0("$",pc_filtered$EXPS), "<br>",
+                                 "Year: 2018")) %>% 
+  addLegend(pal = pal_rev, values = pc_filtered$TOTREV, opacity = 0.9,
+            title = "Total Revenue Percentile")
 
 
 
 # color by total donations/contributions (CONT)
-pal_cont <- colorBin(palette = "RdYlBu", reverse = TRUE, domain = pc_filtered$CONT, 
-                     bins = c(0, 100000, 200000, 300000, 400000, 500000, 200000000)
-                     )
-# pal_cont <- colorQuantile(palette = "RdYlBu", reverse = TRUE, domain = pc_filtered$CONT, 
-#                      n = 20)
+# pal_cont <- colorBin(palette = "RdYlBu", reverse = TRUE, domain = pc_filtered$CONT,
+#                      pretty = TRUE)
+
+
+pal_cont <- colorQuantile(palette = "RdYlBu", reverse = TRUE, domain = pc_filtered$CONT,
+                     n = 7)
 
 
 leaflet() %>% 
@@ -93,12 +111,21 @@ leaflet() %>%
   addPolygons(data = county_refined, col = "black", fillOpacity = 0, opacity = 0.9
               #popup = paste("County:", county_refined$NAME)
               ) %>% 
-  addCircleMarkers(data = pc_filtered, radius = 2, opacity = 1,
-                   color = ~pal_cont(CONT),
+  addCircleMarkers(data = pc_filtered, radius = 6, opacity = 1,
+                   color = "black",
+                   stroke = TRUE,
+                   weight = 0.2,
+                   fillOpacity = 1,
+                   fillColor = ~pal_cont(CONT),
                    popup = paste("Name:", pc_filtered$NAME, "<br>",
                                  "Address:", pc_filtered$full_address, "<br>",
-                                 "Annual Contributions:", paste0("$", pc_filtered$CONT))) %>% 
-  addLegend(pal = pal_cont, values = pc_filtered$CONT, opacity = 0.9)
+                                 "Major Group:", pc_filtered$NTMAJ10, "<br>",
+                                 "Contributions:", paste0("$", pc_filtered$CONT), "<br>",
+                                 "Revenue:", paste0("$",pc_filtered$TOTREV), "<br>",
+                                 "Expenses:", paste0("$",pc_filtered$EXPS), "<br>",
+                                 "Year: 2018")) %>% 
+  addLegend(pal = pal_cont, values = pc_filtered$CONT, opacity = 0.9,
+            title = "Total Contributions Percentile")
 
 
 # color by total expenses (EXPS)
@@ -119,8 +146,13 @@ leaflet() %>%
                    fillColor = ~pal_exp(EXPS),
                    popup = paste("Name:", pc_filtered$NAME, "<br>",
                                  "Address:", pc_filtered$full_address, "<br>",
-                                 "2018 Expenses:", paste0("$",pc_filtered$EXPS))) %>% 
-  addLegend(pal = pal_exp, values = pc_filtered$EXPS, opacity = 0.9)
+                                 "Major Group:", pc_filtered$NTMAJ10, "<br>",
+                                 "Contributions:", paste0("$", pc_filtered$CONT), "<br>",
+                                 "Revenue:", paste0("$",pc_filtered$TOTREV), "<br>",
+                                 "Expenses:", paste0("$",pc_filtered$EXPS), "<br>",
+                                 "Year: 2018")) %>% 
+  addLegend(pal = pal_exp, values = pc_filtered$EXPS, opacity = 0.9,
+            title = "Total Expenses Percentile")
 
 
 
